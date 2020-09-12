8d337c47386bc3601d741702889efd0557c01bfa
ArtoriaRen <s200652168@hotmail.com>
2020-09-11 23:18:04 -0400

    generate dependency file. Each line is a tx and its latest prereq tx

--- src/tx_placement/tx_placer.h
+++ src/tx_placement/tx_placer.h
@@ -24,6 +24,56 @@ extern int lastAssignedAffinity;
 //extern uint32_t txStartBlock;
 //extern uint32_t txEndBlock;
 
+class TxIndexOnChain {
+public:
+    uint32_t block_height;
+    uint32_t offset_in_block;
+
+    TxIndexOnChain();
+    TxIndexOnChain(const uint32_t block_height_in, const uint32_t offset_in_block_in);
+    bool IsNull();
+
+    template<typename Stream>
+    void Serialize(Stream& s) const{
+	s.write(reinterpret_cast<const char*>(&block_height), sizeof(block_height));
+	s.write(reinterpret_cast<const char*>(&offset_in_block), sizeof(offset_in_block));
+    }
+
+    template<typename Stream>
+    void Unserialize(Stream& s) {
+	s.read(reinterpret_cast<const char*>(&block_height), sizeof(block_height));
+	s.read(reinterpret_cast<const char*>(&offset_in_block), sizeof(offset_in_block));
+    }
+
+    friend bool operator<(TxIndexOnChain a, TxIndexOnChain b)
+    {
+        return a.block_height < b.block_height || 
+		(a.block_height == b.block_height && a.offset_in_block < b.offset_in_block);
+    }
+
+    std::string ToString();
+};
+
+class DependencyRecord {
+public:
+    TxIndexOnChain tx;
+    TxIndexOnChain latest_prereq_tx;
+
+    DependencyRecord(const uint32_t block_height, const uint32_t offset_in_block, const TxIndexOnChain& latest_prereq_tx_in);
+
+    template<typename Stream>
+    void Serialize(Stream& s) const{
+	tx.Serialize(s);
+	latest_prereq_tx.Serialize(s);
+    }
+
+    template<typename Stream>
+    void Unserialize(Stream& s) {
+	tx.Unserialize(s);
+	latest_prereq_tx.Unserialize(s);
+    }
+};
+
 class TxPlacer{
 public:
     std::map<uint, std::map<uint, uint>> shardCntMap; // < input_utxo_count, shard_count, tx_count>
@@ -48,6 +98,10 @@ uint32_t sendAllTailTx(int txSendPeriod);
 
 /* return true if the tx is sent, false if the tx is queued. */
 bool sendTx(const CTransactionRef tx, const uint idx, const uint32_t block_height);
+
+inline std::string getDependencyFilename() {
+    return "dependency.out";
+}
 
 #endif /* TX_PLACER_H */
 
